4626cdde954ca6986ab340f2f26fa1c41949a196
FatGeek <yuhrenhuang@gmail.com>
2020-03-06 15:22:30 +0800

    Update page_menu.js

--- scripts/page_menu.js
+++ scripts/page_menu.js
@@ -1,28 +1,48 @@
-$(document).ready(function(){
-autoHoverMenu();
-
-$('.mobile_menu_btn').on('click', function(){
-$('.menu_blcok').toggleClass('show');
-});
+$(document).ready(function() {
+  w3.includeHTML(function() {
+    autoHoverMenu();
+    mobileMenuSwitch();
+  });
 });
 
 //menu auto hover
-function autoHoverMenu(){
-	for(var i=0;i<$('.menu_blcok').find('a').length;i++){
-		if( window.location.pathname.match( $('.menu_blcok').find('a').eq(i).attr('href') ) ){
-			if( $('.menu_blcok').find('a').eq(i).parent().prop('className').match(/menu_level2/g) ){
-				$('.menu_blcok').find('a').eq(i).parent().siblings('.menu').addClass('hover');
-			}else{
-				$('.menu_blcok').find('a').eq(i).addClass('hover');
-			}
-		}
-	}
+function autoHoverMenu() {
+  for (var i = 0; i < $(".menu_blcok").find("a").length; i++) {
+    if (
+      window.location.pathname.match(
+        $(".menu_blcok")
+          .find("a")
+          .eq(i)
+          .attr("href")
+      )
+    ) {
+      if (
+        $(".menu_blcok")
+          .find("a")
+          .eq(i)
+          .parent()
+          .prop("className")
+          .match(/menu_level2/g)
+      ) {
+        $(".menu_blcok")
+          .find("a")
+          .eq(i)
+          .parent()
+          .siblings(".menu")
+          .addClass("hover");
+      } else {
+        $(".menu_blcok")
+          .find("a")
+          .eq(i)
+          .addClass("hover");
+      }
+    }
+  }
 }
 
-//header menu
-// function mobileMenuSwitch(){
-// 	$('.mobile_menu_btn').on('click', function(){
-// 		$('.menu_blcok').toggleClass('show');
-// 	});
-// }
-
+// header menu
+function mobileMenuSwitch() {
+  $(".mobile_menu_btn").on("click", function() {
+    $(".menu_blcok").toggleClass("show");
+  });
+}
